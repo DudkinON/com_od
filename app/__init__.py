7b31eb510ef113fb5c19c4ff38505a63fea0e3ea
Oleg Dudkin <dudkin.on@gmail.com>
2018-01-28 05:16:50 -0600

    feat: Add route for works

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,21 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, jsonify
-from data import get_certificates
+from data import get_certificates, get_works
 
 app = Flask(__name__)
 
 
 @app.route("/certificates")
-def certificates():
-    certs = [item.serialize for item in get_certificates()]
-    return jsonify(certs), 200
+def show_certificates():
+    certificates = [item.serialize for item in get_certificates()]
+    return jsonify(certificates), 200
+
+
+@app.route("/works")
+def show_works():
+    works = [item.serialize for item in get_works()]
+    return jsonify(works), 200
 
 
 if __name__ == '__main__':
