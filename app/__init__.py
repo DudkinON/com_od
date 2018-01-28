2c6a204bed138251328e9e324c82c0a865718105
Oleg Dudkin <dudkin.on@gmail.com>
2018-01-28 04:46:25 -0600

    feat: Add app run

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
-from flask import Flask
-from settings import db
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, jsonify
+from data import get_certificates
 
 app = Flask(__name__)
 
 
 @app.route("/certificates")
 def certificates():
-    return ""
+    certs = [item.serialize for item in get_certificates()]
+    return jsonify(certs), 200
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
